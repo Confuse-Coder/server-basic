a52a9425989c5bacfff438fc78b00b1d65a4aa71
tuhuynh <tuhuynh1902@gmail.com>
2022-05-14 19:24:42 +0700

    video 8 Routing and MVC

--- src/server.js
+++ src/server.js
@@ -1,18 +1,14 @@
 import express from 'express';
 import configViewengine from './configs/viewEngine';
+import initWebRoute from './route/web';
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 configViewengine(app);
-app.get('/', (req, res) => {
-  res.render('index.ejs');
-});
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'));
-});
+initWebRoute(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
